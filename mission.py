3689191d4c82acc96f665f2c2ac2de5d68654378
sedakaraatas <sedakaraatas5@gmail.com>
2022-09-27 22:58:22 +0300

    wp added

--- mission.py
+++ mission.py
@@ -4,7 +4,7 @@ address = 'udpin:localhost:14551'
 vehicle= mavutil.mavlink_connection(address,baudrate=57600,autoreconnect= True)
 vehicle.wait_heartbeat()
 print("baglanti basarili")
-mavwp.MAVWPLoader()
+wp= mavwp.MAVWPLoader()
 def get_alt():
     message = vehicle.recv_match(type='GLOBAL_POSITION_INT', blocking= True)
     alt=message.relative_alt
